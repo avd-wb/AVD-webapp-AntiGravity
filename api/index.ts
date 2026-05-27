92649d9ec92a4a1510745238067a387836c9af64
zoe-the-happiness <zoethehappiness@gmail.com>
2026-05-27 07:31:20 +0530

    chore: restore clean static import of server in api/index.ts since boot blockers are fully resolved

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,2 @@
-import express from "express";
-
-const app = express();
-
-app.all("*", async (req: any, res: any) => {
-  try {
-    const server = await import("../server.js");
-    return server.app(req, res);
-  } catch (err: any) {
-    res.status(500).json({
-      error: "BOOT_CRASH",
-      message: err.message,
-      stack: err.stack ? err.stack.split("\n") : []
-    });
-  }
-});
-
+import { app } from "../server";
 export default app;
